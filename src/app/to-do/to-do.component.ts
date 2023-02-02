a7a1a0a3403cad3287635c73850ab4f907456f38
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2023-02-02 13:47:24 -0500

    Create reactive form for to-do list.

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,26 +1,36 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
   styleUrls: ['./to-do.component.css']
 })
 export class ToDoComponent {
-  public items: string[] = [];
 
-  public newTask!: string;
+  // public items: string[] = [];
 
-  public addToList() {
-    if (this.newTask == ''){
-    }
-    else{
-      this.items.push(this.newTask);
-      this.newTask = '';
-    }
+  // public newTask!: string;
+
+  // public addToList() {
+  //   if (this.newTask == ''){
+  //   }
+  //   else{
+  //     this.items.push(this.newTask);
+  //     this.newTask = '';
+  //   }
+  // }
+
+  // public deleteTask(index: number) {
+  //   this.items.splice(index,1);
+  // }
+
+  todoList = new FormGroup({
+    task: new FormControl(),
+    timeEstimation: new FormControl(),
+    priority: new FormControl()
+  })
+
+  onSubmit(){
+    console.log(this.todoList.value);
   }
-
-  public deleteTask(index: number) {
-    this.items.splice(index,1);
-  }
-
 }
