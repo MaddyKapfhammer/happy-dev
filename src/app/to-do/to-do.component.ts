dc26b4116daa09f5cd71db2b7b8f868bda543ef5
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2023-02-03 13:30:01 -0500

    Modify to use angular reactive form and add components to list.

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {NgModule} from '@angular/core';
+
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
   styleUrls: ['./to-do.component.css']
 })
 export class ToDoComponent {
+  public todoList: FormGroup;
+  task: string = '';
+  timeEstimation: Number = 0;
+  priority: string = '';
+  taskList: string[] = [];
 
+  constructor(private fb:FormBuilder){
+    this.todoList = this.fb.group({
+      task:'',
+      timeEstimation: 0,
+      priority: ''
+    });
+  }
+
+  onSubmit(){
+    console.log(this.todoList.value);
+    this.task = this.todoList.get('task')?.value;
+    this.taskList.push(this.task);
+    console.log(this.taskList);
+    this.timeEstimation = this.todoList.get('task')?.value;
+    this.priority = this.todoList.get('task')?.value;
+  }
   // public items: string[] = [];
 
   // public newTask!: string;
@@ -24,13 +47,11 @@ export class ToDoComponent {
   //   this.items.splice(index,1);
   // }
 
-  todoList = new FormGroup({
-    task: new FormControl(),
-    timeEstimation: new FormControl(),
-    priority: new FormControl()
-  })
+  // todoList = new FormGroup({
+  //   task: new FormControl(),
+  //   timeEstimation: new FormControl(),
+  //   priority: new FormControl()
+  // })
 
-  onSubmit(){
-    console.log(this.todoList.value);
-  }
+
 }
