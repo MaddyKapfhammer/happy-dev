6d7d0f7c624f808b9b7f109b4cc9ef28c7789cf7
MaddyKapfhammer <maddy.kapfhammer@gmail.com>
2023-02-06 14:31:12 -0500

    Create initial code for stopwatch.

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgModule} from '@angular/core';
 
+
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-to-do',
@@ -13,6 +14,11 @@ export class ToDoComponent {
   timeEstimation: Number = 0;
   priority: string = '';
   taskList: string[] = [];
+  counter: undefined | number = 0;
+  timerRef: any;
+  running: boolean = false;
+  startText: string = 'Check-In';
+  timeText: string = '';
 
   constructor(private fb:FormBuilder){
     this.todoList = this.fb.group({
@@ -34,4 +40,30 @@ export class ToDoComponent {
     this.timeEstimation = this.todoList.get('task')?.value;
     this.priority = this.todoList.get('task')?.value;
   }
+
+  timeConverter(milliseconds: number){
+    var hours = Math.round(milliseconds/3600000);
+    var minutes = Math.round(milliseconds/60000);
+    var seconds = Math.round(milliseconds/1000)
+    return hours + ':' + minutes + ':' + seconds;
+  }
+
+  startTimer(){
+    this.running = !this.running;
+    if(this.running){
+      this.startText = 'Check-Out';
+      const startTime = Date.now() - (this.counter || 0);
+      this.timerRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+        this.timeText = this.timeConverter(this.counter);
+      });
+    } else{
+      this.startText = 'Check-In';
+      clearInterval(this.timerRef);
+    }
+  }
+
+  // ngOnDestroy(){
+  //   clearInterval(this.timerRef);
+  // }
 }
